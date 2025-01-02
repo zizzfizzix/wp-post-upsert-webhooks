7fd8b7ac073eafaf35f13e1546a26c0944713ce0
Kuba Serafinowski <kuba.serafinowski@gmail.com>
2025-01-02 13:57:28 -0500

    init plugin

---
+++ wp-post-upsert-webhook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Post Upsert Webhook
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-post-upsert-webhook
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Post_Upsert_Webhook
+ */
+
+// Your code starts here.
